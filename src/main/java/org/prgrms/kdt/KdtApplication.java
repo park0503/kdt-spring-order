c9de7f12991987de309729e1444298e39b5fe01e
ParkSang <hooo0503@gmail.com>
2022-04-08 19:29:55 +0900

    W4D5.6 ~ W4D5.10 SpringBoot

--- src/main/java/org/prgrms/kdt/KdtApplication.java
+++ src/main/java/org/prgrms/kdt/KdtApplication.java
@@ -1,10 +1,13 @@
 package org.prgrms.kdt;
 
 import org.prgrms.kdt.order.OrderItem;
+import org.prgrms.kdt.order.OrderProperties;
 import org.prgrms.kdt.order.OrderService;
 import org.prgrms.kdt.voucher.FixedAmountVoucher;
 import org.prgrms.kdt.voucher.JdbcVoucherRepository;
 import org.prgrms.kdt.voucher.VoucherRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -16,9 +19,9 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.prgrms.kdt.voucher", "org.prgrms.kdt.order", "org.prgrms.kdt.configuration"})
+@ComponentScan(basePackages = {"org.prgrms.kdt.voucher", "org.prgrms.kdt.order"})
 public class KdtApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(KdtApplication.class);
 	public static void main(String[] args) {
 		var spirngApplication = new SpringApplication(KdtApplication.class);
 		//spirngApplication.setAdditionalProfiles("local");
@@ -31,8 +34,15 @@ public class KdtApplication {
 		System.out.println(MessageFormat.format("is Jdbc Repo -> {0}", voucherRepository instanceof JdbcVoucherRepository));
 		System.out.println(MessageFormat.format("is Jdbc Repo -> {0}", voucherRepository.getClass().getCanonicalName()));
 
+		var orderProperties = applicationContext.getBean(OrderProperties.class);
+		logger.info("logger name => {}", logger.getName());
+		logger.warn("version -> {}", orderProperties.getVersion());
+		logger.info("moa -> {}", orderProperties.getMinimumOrderAmount());
+		logger.error("sv -> {}", orderProperties.getSupportVendors());
+		logger.info("d -> {}", orderProperties.getDescription());
+
 		var orderService = applicationContext.getBean(OrderService.class);
-		var order = orderService.createOrder(customerId, new ArrayList<OrderItem>() {{
+		var order = orderService.createOrder(customerId, new ArrayList<>() {{
 			add(new OrderItem(UUID.randomUUID(), 100L, 1));
 		}}, voucher.getVoucherid());
 
