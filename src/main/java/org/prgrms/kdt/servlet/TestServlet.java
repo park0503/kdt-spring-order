6dfbf1c8844a027007c50fda8c2757df51e397bc
ParkSang <hooo0503@gmail.com>
2022-04-19 14:43:28 +0900

    W6D1.6 servlet 실습 2/2

--- src/main/java/org/prgrms/kdt/servlet/TestServlet.java
+++ src/main/java/org/prgrms/kdt/servlet/TestServlet.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
